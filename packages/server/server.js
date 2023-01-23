408a33de5572106969464d98f5696edbbdec1f58
Tim Quattrochi <timq82@gmail.com>
2023-01-22 20:55:53 -0500

    production prep, 404 middleware

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { PORT } = require("./config/constants");
+const { PORT, API } = require("./config/constants");
 const { errorHandler } = require("./middleware/errorMiddle");
 const connectMyDB = require("./config/db");
 
@@ -13,8 +13,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/posts", require("./routes/postRoutes"));
+app.use(`${API}/users`, require("./routes/userRoutes"));
+app.use(`${API}/posts`, require("./routes/postRoutes"));
+
+//404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, that page can't be found.");
+});
+
+app.use(API_URL, apiRoutes);
+// add the following
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.all("*", (req, res, next) => {
+    res.sendFile(
+      path.resolve(__dirname, "../client/build/index.html")
+    );
+  });
+}
 
 app.use(errorHandler);
 
