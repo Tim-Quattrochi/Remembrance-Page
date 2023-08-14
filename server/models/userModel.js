67605215dda989db18d821530263b59729b7c0c8
Tim Quattrochi <timq82@gmail.com>
2023-08-13 21:17:56 -0400

    added timestamps, changed role, password not required on user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  uid: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  picture: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: [true, "You must enter a password."],
-  },
-
-  roles: [
-    {
+const userSchema = mongoose.Schema(
+  {
+    uid: {
       type: String,
-      enum: ["admin", "user"],
+    },
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    picture: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+
       required: true,
       default: "user",
     },
-  ],
-});
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
