7b19f7f48ba9879c9d756b2602770a29bb00ed44
Tim Quattrochi <timq82@gmail.com>
2023-08-17 18:17:40 -0400

    moving some inline styles to their own css files

--- client/src/components/Posts/PostCard/PostCard.jsx
+++ client/src/components/Posts/PostCard/PostCard.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Card,
-  Container,
-  OverlayTrigger,
-  Tooltip,
-} from "react-bootstrap";
+import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { FaHeart } from "react-icons/fa";
 import { FcLikePlaceholder } from "react-icons/fc";
 import { formatDate } from "../../../utils.js/date";
@@ -13,46 +8,17 @@ import "./postCard.css";
 const PostCard = ({ post, likes, handleLike, user, index }) => {
   return (
     <div key={post._id}>
-      <div
-        style={{
-          fontFamily: "Montserrat",
-          fontWeight: "Regular",
-          color: "rgba(0,0,0,0.4)",
-          fontSize: "16px",
-        }}
-      >
-        {post.user.name}
-      </div>
+      <div className="user-name">{post.user.name}</div>
       <Card
         bg="none"
         key={post._id}
         className="mb-2"
-        border="6px 6px 6px 6px"
         style={{
-          border: "none",
-
           backgroundColor: index % 2 === 0 ? "#CCF1E1" : "white",
         }}
       >
-        <Card.Body
-          style={{
-            fontFamily: "Montserrat",
-            fontWeight: "Regular",
-            color: "rgba(0,0,0,0.7)",
-            maxWidth: "100%",
-            outline: "none",
-
-            padding: "20px",
-          }}
-        >
-          <Card.Text
-            style={{
-              wordWrap: "break-word",
-              whiteSpace: "pre-line",
-            }}
-          >
-            {post.content}
-          </Card.Text>
+        <Card.Body className="card-body">
+          <Card.Text>{post.content}</Card.Text>
         </Card.Body>
 
         {post.likes && post.likes.length >= 0 && (
@@ -92,18 +58,7 @@ const PostCard = ({ post, likes, handleLike, user, index }) => {
           </OverlayTrigger>
         )}
       </Card>
-      <div
-        style={{
-          fontSize: "10px",
-          color: "#A8A8A8",
-          fontFamily: "Montserrat",
-          fontWeight: "Regular",
-          margin: 0,
-          marginTop: "10px",
-        }}
-      >
-        {formatDate(post.createdAt)}
-      </div>
+      <div className="date">{formatDate(post.createdAt)}</div>
     </div>
   );
 };
--- client/src/pages/LandingPage/LandingContent.jsx
+++ client/src/pages/LandingPage/LandingContent.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const LandingContent = () => {
   return (
-      <div className='landing-card-container'><div className="card-header">Tribute to Jerry Krikava</div><div className="card-body">
-          I am honored to present my tribute page for my beloved
-          brother, Jerry Krikava.
-      </div><div className="card-section-header">Jerry's Life</div><div className="card-section-body">
-              {" "}
-              <p className="personalPara">
-                  Jerry was born on March 9th, 1990 and unfortunately passed
-                  away in March 2021. He had non-verbal autism and was a
-                  huge fan of 80's music, particularly Madonna and Journey.
-                  Purpose of this website In honor of Jerry's memory, I have
-                  created this website as a way for friends and family to
-                  come together and share their memories of Jerry.
-              </p>
-              <p className="personalPara">
-                  Visitors can post on Jerry's guest book and view photos of
-                  him. Keeping Jerry's Memory Alive This website is a way
-                  for us to keep Jerry's memory alive and to remember the
-                  joy and love he brought into our lives. I hope that this
-                  website will be a source of comfort and support for those
-                  who knew and loved Jerry.
-              </p>
-          </div></div>
-  )
-}
+    <div className="landing-card-container">
+      <div className="card-header">Tribute to Jerry Krikava</div>
+      <div className="card-body">
+        I am honored to present my tribute page for my beloved
+        brother, Jerry Krikava.
+      </div>
+      <div className="card-section-header">Jerry's Life</div>
+      <div className="card-section-body">
+        {" "}
+        <p className="personalPara">
+          Jerry was born on March 9th, 1990 and unfortunately passed
+          away in March 2021. He had non-verbal autism and was a huge
+          fan of 80's music, particularly Madonna and Journey. Purpose
+          of this website In honor of Jerry's memory, I have created
+          this website as a way for friends and family to come
+          together and share their memories of Jerry.
+        </p>
+        <p className="personalPara">
+          Visitors can post on Jerry's guest book and view photos of
+          him. Keeping Jerry's Memory Alive This website is a way for
+          us to keep Jerry's memory alive, and to remember the joy and
+          love he brought into our lives. I hope that this website
+          will be a source of comfort and support for those who knew
+          and loved Jerry.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default LandingContent
+export default LandingContent;
