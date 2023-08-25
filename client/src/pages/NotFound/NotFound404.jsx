3b9ae67b2426780c4dcf7bf4847294a42bf5266e
Tim Quattrochi <timq82@gmail.com>
2023-08-25 14:50:33 -0400

    404 not found page and styling.

--- client/src/pages/NotFound/NotFound404.jsx
+++ client/src/pages/NotFound/NotFound404.jsx
@@ -1,20 +1,32 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { BsQuestionLg } from "react-icons/bs";
+import "./notFound.css";
+import { PrimaryBtn } from "../../components";
 
 const NotFound404 = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
 
   useEffect(() => {
     console.log(`No routes found for ${location.pathname}`);
   }, [location.pathname]);
 
   return (
-    <div style={{ height: "100vh", marginTop: "50px" }}>
+    <div className="not-found-container">
       <h1>
         Sorry, the page you are looking for is not found.{" "}
         <BsQuestionLg fill="#C8F3DF" />
       </h1>
+      <PrimaryBtn
+        onClick={handleClick}
+        text="Go Back"
+        style="reg-btn"
+      />
     </div>
   );
 };
